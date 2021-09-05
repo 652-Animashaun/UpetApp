36a73d867834cac7d4e736799804bb1e50358996
Muizz <anmshmz@gmail.com>
2021-09-05 13:14:16 +0100

    first branch commit for api endpoint feature development

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, session, render_template, request, jsonify
+from flask import Flask, session, render_template, request, jsonify, make_response, url_for
 from sqlalchemy import create_engine, exc
 import json
 
@@ -26,4 +26,45 @@ def index():
 	page = request.args.get('page', 1, type=int)
 
 	users=github_users.query.paginate(page=page, per_page=results_per_page)
+	print(users)
 	return render_template('index.html', users=users)
+
+
+### API ENDPOINT
+
+@app.route("/api/users/profiles", methods=['GET'])
+def get_users():
+	user_list = []
+
+	if request.args.get('results_per_page') == 'None' or request.args.get('results_per_page') == None :
+		results_per_page=25
+		
+	else:
+		### retrieve query parameter from url 
+		results_per_page=int(request.args.get('results_per_page'))
+
+		
+
+	page = request.args.get('page', 1, type=int)
+	users=github_users.query.paginate(page=page, per_page=results_per_page)
+	
+	for user in users.items:
+		user_dict={}
+		user_dict['username']= user.username
+		user_dict['git_id']=user.git_id
+		user_dict['avatar_url']=user.avatar_url
+		user_dict['url']=user.url
+		user_dict['type']=user.user_type
+		user_list.append(user_dict)
+
+	### make_response with jsonified userlist and send nextpage link with response header for pagination
+
+	response = make_response(jsonify({
+		'users': user_list
+		}),
+	200,
+	)
+	response.headers["nextpage"] = f"http://127.0.0.1:5000/api/users/profiles?page={users.next_num}&results_per_page={request.args.get('results_per_page')}" 
+		
+	return response
+
